5a2aa23374570c3c83dd69e93e067ea263fc28c7
liuzhensong <liuzhensong@loongson.cn>
2022-05-17 12:34:25 +0800

    LoongArch: Code format.

--- bfd/elfxx-loongarch.c
+++ bfd/elfxx-loongarch.c
@@ -1191,7 +1191,11 @@ bool loongarch_gen_adjust_reloc_bits (reloc_howto_type *howto, bfd_vma *fix_val)
   return true;
 }
 
-/* Reloc type R_LARCH_SOP_POP_32_S_10_16_S2.  */
+/* Reloc type :
+ * R_LARCH_SOP_POP_32_S_10_16_S2
+ * R_LARCH_B16.
+*/
+
 bool loongarch_adjust_reloc_bits_l16 (reloc_howto_type *howto,
 					bfd_vma *fix_val)
 {
@@ -1231,7 +1235,10 @@ bool loongarch_adjust_reloc_bits_l16 (reloc_howto_type *howto,
   return true;
 }
 
-/* Reloc type R_LARCH_SOP_POP_32_S_0_5_10_16_S2.  */
+/* Reloc type :
+ * R_LARCH_SOP_POP_32_S_0_5_10_16_S2
+ * R_LARCH_B21.
+*/
 bool loongarch_adjust_reloc_bits_l16_xx5_h5 (reloc_howto_type *howto,
 					     bfd_vma *fix_val)
 {
@@ -1271,7 +1278,11 @@ bool loongarch_adjust_reloc_bits_l16_xx5_h5 (reloc_howto_type *howto,
   return true;
 }
 
-/* Reloc type R_LARCH_SOP_POP_32_S_0_10_10_16_S2.  */
+/* Reloc type:
+ *  R_LARCH_SOP_POP_32_S_0_10_10_16_S2
+ *  R_LARCH_B26
+ *  R_LARCH_BL26.
+*/
 bool loongarch_adjust_reloc_bits_l16_h10 (reloc_howto_type *howto,
 					  bfd_vma *fix_val)
 {
